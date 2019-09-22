22d07024a449b13ba017f2629b64148637c6ef2c
Naya  Willis <greedybrain18@gmail.com>
2019-09-22 19:57:56 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,7 @@
 class Book 
   
   attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
   def initialize(title)
     @title = title
@@ -8,30 +9,6 @@ class Book
   
   def title
     @title
-  end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(page_count)
-    @page_count = page_count
-  end
-  
-  def page_count 
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre 
-    @genre
   end
   
   def turn_page
